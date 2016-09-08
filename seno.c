3a910d13b9b14bd31390647b3f999361b88d6118
xibelly <xibeline@gmail.com>
2016-09-08 15:38:41 -0500

    first commit: try1

---
+++ seno.c
@@ -0,0 +1,6 @@
+/* Xibelly Eliseth Mosquera Escobar
+
+ 1) Escribir un programa que utilice la serie de taylor de la funcion seno(alrededor de x=0) para calcular el seno de un angulo. El programa debe recibir como entrada el valor del angulo (x) en grados y en el numero de terminos (N) de la serie a usarse en la expansion, y retornar en pantalla el valor de sen(x).
+
+ */
+
