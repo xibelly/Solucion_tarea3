a687d1f366bd75ec80a51ff494ce82a4fbb8d1ce
xibelly <xibeline@gmail.com>
2016-09-08 23:41:31 -0500

    Add files via upload

---
+++ makefile
@@ -0,0 +1,28 @@
+CC = gcc
+
+CFLAGS = -c -O0 -I/home/$(USER)/local/include/ -I/usr/include/  
+
+CFLAGSDEBUG = -g -Wall -c -I/home/$(USER)/local/include/ -I/usr/include/
+
+LFLAGS = -lm -L/home/$(USER)/local/lib -Wl,-R /home/$(USER)/local/lib 
+
+
+seno:
+	echo Estoy compilando $@.c
+	$(CC) $(CFLAGS) $@.c -o $@.o
+	$(CC) $@.o $(LFLAGS) -lgsl -lgslcblas -lfftw3 -lm -o  $@.x		
+
+debug: 
+	echo Estoy compilando para debugear $@.c
+	$(CC) $(CFLAGSDEBUG) $@.c -o $@.o
+	$(CC) $@.o $(LFLAGS) -lgsl -lgslcblas -lfftw3 -o $@.x
+
+
+clean:
+	rm -rf *.~
+	rm -rf *.out
+	rm -rf *#
+	rm -rf *.o
+	rm -rf *.a
+	rm -rf *.so
+	rm *.x
--- seno.c
+++ seno.c
@@ -4,3 +4,78 @@
 
  */
 
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAX 20 //max numero de digitos de salida 
+
+
+//////////////////////Funciones////////////////////////
+
+factorial(int n)
+{
+  int res[MAX],i;
+  int x, res_size;
+  
+  res[0] = 1;
+  res_size = 1;
+
+  for(x=2; x<=n; x++)
+    {
+      res_size = multiply(x, res, res_size);
+    }
+
+  //for(i=res_size -1 ; i>=0; i--)
+  //cout << res[i];
+
+  
+}
+
+int multiply(int x, int res[], int res_size)
+{
+  int carry,i,prod;
+
+  carry = 0;
+
+  for(i=0; i<res_size; i++)
+    {
+      prod   = res[i] * x + carry;
+      res[i] = prod % 10;
+      carry  = prod/10;
+
+    }
+  while(carry)
+    {
+      res[res_size] = carry%10;
+      carry         = carry/10;
+      res_size++;
+    }
+
+
+  return res_size;
+}
+
+
+////////////////////////////Programa principal///////////////////
+int main(int argc, char **argv){
+
+  int N,i,term;
+  double angulo,numerador,seno_x;
+  
+  angulo = atof(argv[2]);
+  N      = atoi(argv[3]);
+  
+  for(i=0; i<N; i++)
+    {
+      numerador    = (-1)^(i) * (angulo)^(2*i+1);
+
+      term         = (2*i) + 1;       
+             
+      //seno_x       = numerador / factorial(term); 
+
+      seno_x        = seno_x + numerador / factorial(term); 
+    }
+  printf("seno x:%lf\n",seno_x);
+
+
+}
