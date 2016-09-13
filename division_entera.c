ad445e9b1257010d24a7bdbb038c48d29f3bc4b8
xibelly <xibeline@gmail.com>
2016-09-12 23:06:53 -0500

    Update division_entera.c

--- division_entera.c
+++ division_entera.c
@@ -2,6 +2,26 @@
 
 3) Division entera. Escriba un programa que calcule el numero de veces que se puede dividir un numero entre dos.
  */
+ 
+/*
+Analisisi y diseño
+
+Nuestro problema es e l siguiente:
+
+	a/2 = b + r;    	b: resultado  r:residuo
+
+Para llevar  a dicha operacion necesitamos:
+
+-pasar  el # que sera dividido, esto se hara por linea de comando
+
+-al hacer la division por dos vamos a establecer que se tome la parte inferior de la operacion, para ello hacemos uso de "floor".
+Esta operacion se realiza un numero n de veces hasta que se cumpla que el resultado es 1 y el residuo es 0
+
+-calcular el numero n de operaciones que se realizan e imprimir en patalla 
+
+*/
+
+
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -36,5 +56,7 @@ int main(int argc, char **argv){
   
 
   printf("divison entera %d\n",suma);
+  
+  return 0;
   
 }
