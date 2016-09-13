5d96d78162af782898113068e484e53f207622ee
xibelly <xibeline@gmail.com>
2016-09-12 23:15:59 -0500

    Update factorial_de_x.c

--- factorial_de_x.c
+++ factorial_de_x.c
@@ -1,3 +1,26 @@
+/*
+Xibelly Eliseth Mosquera Escobar 
+
+4) Escriba un programa que calcule el factorial de un numero x.
+
+*/
+
+
+/*
+  Analisis y diseño
+  
+  Nuestro problema es el de calcular el factorial de un #
+  
+  n ! = (n -1)* n
+  
+  Como en problemas anteriores se hizo uso de la funcion factorial en este caso tambien lo haremos, de tal modo que 
+  llamamos dicha funcion en las directivas de pre-procesamiento, y unicamente debemos entregar el # al que se le desea saber
+  el factorial, esto se hara a traves de linea de comandos
+
+*/
+
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include"factorial.c"
